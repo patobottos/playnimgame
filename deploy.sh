b6e5f47df60133d2da7dca0c421cff8b35905e12
Patricio Bottos <patobottos@gmail.com>
2023-10-18 09:43:18 +0200

    Deployin site

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# Abort on errors
+set -e
+
+# Build
+npm run build
+
+# Navigate into the build output directory
+cd dist
+
+# Place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# If you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# If you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# If you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:patobottos/playnimgame.git main:gh-pages
+
+cd -
